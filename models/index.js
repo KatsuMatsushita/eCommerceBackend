ce2c9691d687a299f953ba613e691ccda7d859a1
Hungry Man <katsu.matsushita@gmail.com>
2021-09-26 10:50:19 -0400

    Model index.js written The index.js for the models is written

--- models/index.js
+++ models/index.js
@@ -4,14 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// These are association methods to be executed on the models
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.hasMany(Tag, {
+  through: "ProductTag",
+  foreignKey: "id"
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.hasMany(Product, {
+  through: "ProductTag",
+  foreignKey: "id"
+})
 module.exports = {
   Product,
   Category,
