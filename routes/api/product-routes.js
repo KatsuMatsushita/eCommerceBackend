53043d2f1c7f64f41b8cf231034e6c502cbdfc43
Hungry Man <katsu.matsushita@gmail.com>
2021-09-27 16:39:01 -0400

    get one product completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,13 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: 
+        [
+          {model: Category},
+          {model: Tag, through: ProductTag},
+        ]
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -20,9 +26,19 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findbyPk(req.params.id,{
-      include: [{model: Category, Tag}],
+    const productData = await Product.findByPk(req.params.id, {
+      include: 
+      [
+        {model: Category}, 
+        {model: Tag, through: ProductTag}
+      ]
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
